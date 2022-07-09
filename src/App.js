15ee4403bfbcb9e1bcc2142b1fdd9f361c0f0293
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-07-09 08:03:44 +0530

    css added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import MyProjects from './components/MyProjects';
+import Contact from './components/Contact';
+import './App.css';
+
+
+import{ Box } from '@mui/material';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box 
+      border='2px solid green'
+      m='auto'
+      sx={{
+        width:{
+          xl:'1480px'
+        } 
+
+      }}>App</Box>
   )
 }
 
