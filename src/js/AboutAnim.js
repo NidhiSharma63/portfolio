49f9bfd63409a2ef7ad71c6f0597a0adacb7b18e
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-07-16 21:20:40 +0530

    AboutWrapper & MybuilAnim replaced by commonAnim

--- src/js/AboutAnim.js
+++ src/js/AboutAnim.js
@@ -1,27 +1,5 @@
 
 
-export const AboutWrapperAnim = ({selector,classes}) =>{
-  const Item = document.querySelector(`.${selector}`);
-
-  let options = {
-    root: document.querySelector('#scrollArea'),
-    threshold: 0.1,
-    rootMargin:'0px 0px 100px 0px'
-  }
-  
-  let observer = new IntersectionObserver(function(
-    entries,
-    observer
-  ) {
-    entries.forEach(entry => {
-      if(!entry.isIntersecting) return;
-      Item.classList.remove(`${classes}`);
-      observer.disconnect();
-    })
-  }, options);
-  observer.observe(Item);
-};
-
 let CommonOptions = {
   root: document.querySelector('#scrollArea'),
   threshold: 0,
--- src/js/Mybuild.js
+++ src/js/Mybuild.js
@@ -1,27 +1,4 @@
 
-export const MyProjectsAnim = () =>{
-  const Item = document.querySelector('.MyProjects');
-
-  let options = {
-    root: document.querySelector('#scrollArea'),
-    threshold: 0.1,
-    rootMargin:'0px 0px 100px 0px'
-  }
-  
-  let observer = new IntersectionObserver(function(
-    entries,
-    observer
-  ) {
-    entries.forEach(entry => {
-      if(!entry.isIntersecting) return;
-      Item.classList.remove('project-wraaper-anim');
-      observer.disconnect();
-    })
-  }, options);
-  observer.observe(Item);
-};
-
-
 export const MyProjectsChildAnim = () =>{
   const Item = document.querySelectorAll('.projects-container');
   let options = {
