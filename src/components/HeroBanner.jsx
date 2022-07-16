75defe3eae0b9f999a14115c55c11c3367495852
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-07-16 11:09:12 +0530

    code split into js components

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -3,35 +3,9 @@ import React, { useEffect } from 'react';
 import { Box,Stack,Button,Typography, ThemeProvider } from '@mui/material';
 import main from '../Images/main2.png';
 import ButtonComponent from './Button';
+import anim from '../js/HeroBanner'
 
 import { heroTypography1,heroTypography2 } from '../materialui/Typrography'
-
-
-const anim = () =>{
-  const heroBaner = document.getElementById('hero-banner-container');
-  const heroBannerText = document.querySelector('.hero-banner');
-  const heroBannerImage = document.querySelector('.hero-banner-image');
-
-  let options = {
-    root: document.querySelector('#scrollArea'),
-    rootMargin: '0px',
-    threshold: 0,
-    rootMargin:'0px 0px -100px 0px'
-  }
-  
-  let observer = new IntersectionObserver(function(
-    entries,
-    observer
-  ) {
-    entries.forEach(entry => {
-      if(!entry.isIntersecting) return;
-      heroBannerText.classList.remove('tranfrom-left');
-      heroBannerImage.classList.remove('tranfrom-image');
-      observer.disconnect();
-    })
-  }, options);
-  observer.observe(heroBaner);
-}
 
 const HeroBanner = () => {
   useEffect(()=>{ 
